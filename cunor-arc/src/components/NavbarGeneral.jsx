dbb8c95e5fe9ffef04eb2f5ce763393e11e1be49
mozart953 <wilsonnianopop@gmail.com>
2023-11-18 18:29:13 -0600

    Estilo del navbar

--- cunor-arc/src/components/NavbarGeneral.jsx
+++ cunor-arc/src/components/NavbarGeneral.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import {getServerSession} from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
@@ -10,80 +11,77 @@ async function Navbar(){
     
 
     return(
-        <nav className="navbar navbar-expand-lg  navbar-dark bg-primary">
-            <div className="container-fluid">
-            <h1 className="navbar-brand" >
-                <Link href='/' className="nav-link">CUNOR- by Wilson Pop</Link>
-                
-            </h1>
-
-                <div className="collapse navbar-collapse" id="navbarNav">
-
-                    <ul className="navbar-nav ml-auto">
-                        {
-                            !sesion?.user?(
-                                <>
-                                <li className="nav-item">
-                                    <Link href='/' className="nav-link" aria-current="page">Página inicial</Link>
-                                </li>
-                
-                
-                        
-                                <li className="nav-item">
-                                    <Link href='/auth/login' className="nav-link">Iniciar sesión</Link>
-                                </li>
-                                
-                                </>
-                                    
-                    
-
-                            ):(
-                                <>
-                                            
-                                    <li className="nav-item">
-                                        <Link href='/dashboardAdmin' className="nav-link">Dashboard</Link>
-                                    </li>
-
-                                    <li className="nav-item">
-                                        <Link href='/dashboardAdmin/adminUsuarios' className="nav-link">Gestión de usuarios</Link>
-                                    </li>
-                        
-                    
-                                    <li className="nav-item">
-                                        <Link href='/dashboardAdmin/registro' className="nav-link">Registro de usuarios</Link>
-                                    </li>
-
-                                    
-                                    <li className="nav-item">
-                                        <Link href='/dashboardAdmin/adminCarreras' className="nav-link">Gestión de carreras</Link>
-                                    </li>
-
-                                    
-                                    <li className="nav-item"> 
-                                        <Link href='/dashboardAdmin/adminRoles' className="nav-link">Roles</Link>
-                                    </li> 
-
-                                    <li className="nav-item"> 
-                                        <Link href='/api/auth/signout' className="nav-link">Cerrar sesion</Link>
-                                    </li>
+        
+        <nav className="navbar navbar-expand navbar-light bg-light">
+            <div className="container">
+                <h1 className="navbar-brand">
+                    <Link href='/' className="nav-link">CUNOR</Link>
+                </h1>
 
 
-                                </>
-                            )
-
-                        }
-
-
-                    
+                <form className="input-group" style={{width: "400px"}}>
+                    <input type="search" className="form-control" placeholder="Buscar documento" aria-label="Search" />
+                    <button className="btn btn-outline-primary" type="button" data-mdb-ripple-color="dark" style={{padding: ".45rem 1.5rem .35rem"}}>
+                        Buscar
+                    </button>
+                </form> 
 
                 
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    {
+                        !sesion?.user ? (
+                        <>
+                            
+                            <li className="nav-item">
+                            <Link href='/' className="nav-link active d-flex flex-column text-center" aria-current="page">
+                                <i className="bi bi-house-door-fill"></i><span className="small">Página inicial</span>
+                            </Link>
+                            </li>
+                            <li className="nav-item">
+                            <Link href='/auth/login' className="nav-link d-flex flex-column text-center">
+                                <i className="bi bi-lock"></i><span className="small">Iniciar sesión</span>
+                            </Link>
+                            </li>
+                        </>
+                        ) : (
+                        <>
+                            <li className="nav-item">
+                                <Link href='/dashboardAdmin' className="nav-link d-flex flex-column text-center">
+                                    <i className="bi bi-pencil-square"></i><span className="small">Dashboard</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href='/dashboardAdmin/adminUsuarios' className="nav-link d-flex flex-column text-center">
+                                    <i className="bi bi-person"></i><span className="small">Gestión de usuarios</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href='/dashboardAdmin/registro' className="nav-link d-flex flex-column text-center">
+                                    <i className="bi bi-person-badge-fill"></i><span className="small">Registro de usuarios</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href='/dashboardAdmin/adminCarreras' className="nav-link d-flex flex-column text-center">
+                                    <i className="bi bi-pin-angle-fill"></i><span className="small">Gestión de carreras</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href='/dashboardAdmin/adminRoles' className="nav-link d-flex flex-column text-center">
+                                    <i className="bi bi-person-lock"></i><span className="small">Roles</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link d-flex flex-column text-center" href='/api/auth/signout'>
+                                    <i className="bi bi-power"></i><span className="small">Cerrar sesion</span>
+                                </Link> 
+                            </li>
+                        </>
+                        )
+                    }
                     </ul>
-
-                </div>
-            
-            </div>    
-
-        </nav>
+            </div>
+      </nav>
+      
         
 
     );
