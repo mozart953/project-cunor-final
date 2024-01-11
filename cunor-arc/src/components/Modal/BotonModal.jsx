2d8b90aed6276f131a43a3d4bb5c5897aaf1603c
mozart953 <wilsonnianopop@gmail.com>
2024-01-11 17:58:23 -0600

    boton para mostrar y ocultar archivos, sincronicidad de datos

--- cunor-arc/src/components/Modal/BotonModal.jsx
+++ cunor-arc/src/components/Modal/BotonModal.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 function MyButton({ onOpenModal }) {
   return (
     <Button variant="primary" onClick={onOpenModal}>
-      <i className="bi bi-bookmarks"></i> Referencia
+      <strong><i className="bi bi-bookmarks"></i> Referencia</strong>
     </Button>
   );
 }
