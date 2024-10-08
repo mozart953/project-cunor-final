7ebf571bc5ce898a9bbbf6802f2d4bd68deb645c
mozart953 <wilsonnianopop@gmail.com>
2024-10-07 19:08:02 -0600

    ordenamiento alfabetico-categoria-interno

--- cunor-arc/src/app/api/datos/reDetalleTrabajoInterno/filtroCategoriasP/route.js
+++ cunor-arc/src/app/api/datos/reDetalleTrabajoInterno/filtroCategoriasP/route.js
@@ -23,6 +23,7 @@ export async function GET(request){
     const searchTerm = paramametros.get('searchTerm')|| '';
     const orderDirection = paramametros.get('orderDirection')|| 'desc';
     const ordenCampo = paramametros.get('orderCampo') || 'nombreCategoria';
+    const letra = paramametros.get('letra') || '';
 
     let whereClause ={};
 
@@ -54,6 +55,16 @@ export async function GET(request){
                 AND: searchConditions
             };
         }
+        
+        if (letra!=='' && letra!==null) {
+            whereClause = {
+                ...whereClause,
+                nombreCategoria: {
+                        startsWith: letra,
+                        mode: 'insensitive'
+                    }
+                }
+        };
 
         const totalItems = await db.categoria.count({
             where:whereClause,
