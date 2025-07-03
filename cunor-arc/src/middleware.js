91f0ef771787baef31c21b74d4129bab461da3b0
mozart953 <wilsonnianopop@gmail.com>
2025-07-03 13:37:04 -0600

    protecting general dashboard

--- cunor-arc/src/middleware.js
+++ cunor-arc/src/middleware.js
@@ -1,4 +1,4 @@
 
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboardAdmin/:path*" , "/dashboardOperador/:path*"] };
+export const config = { matcher: ["/dashboardAdmin/:path*" , "/dashboardOperador/:path*", "/dashboardGeneralUserC/:path*"] };
